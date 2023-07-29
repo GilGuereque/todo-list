afbde6eaaab771f76b58dcc210ac6aa1ea923a99
GilGuereque <gguereque326@gmail.com>
2023-07-28 20:02:21 -0700

    Set up GET request for index.ejs

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const PORT = 5050;
 require('dotenv').config()
 
 
+// Render page using GET function
+app.get('/', function(req, res){
+    res.render('index.ejs')
+});
+
 
 // Setting up app to run on localhost PORT 5050
 app.listen(process.env.PORT || PORT, () => {
