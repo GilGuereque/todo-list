8997939e17e9673574b6f962d502c5ba26608709
GilGuereque <gguereque326@gmail.com>
2023-07-31 17:19:29 -0700

    Updated main.js logic

--- public/js/main.js
+++ public/js/main.js
@@ -18,4 +18,61 @@ Array.from(itemCompleted).forEach((element) => {
 });
 
 // async function to delete the task
-async function()
+async function deleteItem(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('deleteItem', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(error){
+        console.log(error)
+    }
+};
+
+// async function to mark complete 
+async function markComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try {
+        const response = await fetch('markComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(error){
+        console.log(error)
+    }
+};
+
+// async function to mark uncomplete
+async function markUnComplete(){
+    const itemText = this.parentNode.childNodes[1].innerText
+    try{
+        const response = await fetch('markUnComplete', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                'itemFromJS': itemText
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
